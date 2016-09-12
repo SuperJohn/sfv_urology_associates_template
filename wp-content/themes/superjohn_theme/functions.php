8fcd53854a0ee357f814c347b7d7824a6629090f
John Houghton <johnkirkhoughton1@gmail.com>
2016-09-12 12:03:38 -0700

    added styles & scripts to functions.php - kinda works
    the css seems to work, but the page doesn't look quite right.

--- wp-content/themes/superjohn_theme/functions.php
+++ wp-content/themes/superjohn_theme/functions.php
@@ -13,4 +13,23 @@
     }
 
     add_action('after_setup_theme', 'SfvUrologyAssociates_setup');
+
+    function my_scripts() {
+
+        /* add styles */
+        wp_enqueue_style( 'bootstrap-core', get_template_directory_uri() . '/bootstrap.min.css') ;
+        wp_enqueue_style( 'revolution-slider', get_template_directory_uri() . '/revolution-slider.css') ;
+        wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css') ;
+        wp_enqueue_style( 'responsive', get_template_directory_uri() . '/responsive.css') ;
+        wp_enqueue_style( 'bootstrap-margin-padding', get_template_directory_uri() . '/bootstrap-margin-padding.css') ;
+
+        /* add scripts */
+        wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), true );
+        wp_enqueue_script( 'js/respond', get_template_directory_uri() . '/js/respond.js', array('jquery'), true );
+        wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), true );
+    }
+
+    add_action('wp_enqueue_style', 'my_scripts');
+    add_action('wp_enqueue_scripts', 'my_scripts');
+
 ?>
